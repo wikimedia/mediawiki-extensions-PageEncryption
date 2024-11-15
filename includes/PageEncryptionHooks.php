329618f617b56fee6e548e72973808788e52cbca
Umherirrender <umherirrender_de.wp@web.de>
2024-11-15 14:20:42 +0100

    Use explicit nullable type on parameter arguments (for PHP 8.4)
    Implicitly marking parameter $... as nullable is deprecated in PHP
    8.4. The explicit nullable type must be used instead.
    
    Change-Id: I36fb24ca93733a6a5b568de277fdee0b90531935

--- includes/PageEncryptionHooks.php
+++ includes/PageEncryptionHooks.php
@@ -41,7 +41,7 @@ class PageEncryptionHooks {
 	 * @param DatabaseUpdater|null $updater
 	 * @return void
 	 */
-	public static function onLoadExtensionSchemaUpdates( DatabaseUpdater $updater = null ) {
+	public static function onLoadExtensionSchemaUpdates( ?DatabaseUpdater $updater = null ) {
 		$base = __DIR__;
 		$dbType = $updater->getDB()->getType();
 		$tables = [
